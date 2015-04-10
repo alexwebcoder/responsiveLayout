3948b4a0eb64ed7ff569b40ba94a16aa03a35dba
alexwebcoder <alexdeleononline@gmail.com>
2015-04-09 22:20:47 -0700

    4/9/15

--- responsive.js
+++ responsive.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	$(".menu-trigger").click(function() {
 		
 		$("nav").slideToggle(400, function() {
+			
 			$(this).toggleClass("nav-expanded").css({"display" : ""});
 		});
 		
