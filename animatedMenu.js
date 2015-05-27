cbad989bd27935921c4414809138d9a5f58dc516
alexwebcoder <alexdeleononline@gmail.com>
2015-05-27 00:42:53 -0700

    latest commit

---
+++ animatedMenu.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+		
+	$(".menu-trigger").click(function() {
+		
+		$("nav").slideToggle(400, function() {
+			
+			$(this).toggleClass("nav-expanded").css({"display" : ""});
+		});
+		
+	});
+	
+});
